931883920c0123d6ff0b0bc30afc73380dcc053c
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2023-11-01 14:53:27 +0100

    added more containers. Learned about the usage of generics in dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello world!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 27, 5, 65),
+                Color.fromARGB(255, 61, 9, 97)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello world!'),
+          ),
         ),
       ),
     ),
