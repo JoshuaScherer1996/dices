c9c6639b7087285d2790fdff6c59ab0849dfdbd8
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2023-11-05 17:20:48 +0100

    Started cleaning up the code by creating new files for classes, starting with the gradient container

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:dices/gradiant_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-// Class for a custome widget to capsulate the color and text logic.
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 27, 5, 65),
-            Color.fromARGB(255, 61, 9, 97)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello world!',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
-    );
-  }
 }
