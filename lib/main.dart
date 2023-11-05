34573c8ca90ddc4cfc00da3bd63f3d8b4b90d581
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2023-11-05 17:03:52 +0100

    Created a custome widget for my GraidentContainer

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,33 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 27, 5, 65),
-                Color.fromARGB(255, 61, 9, 97)
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello world!'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+// Class for a custome widget to capsulate the color and text logic.
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 27, 5, 65),
+            Color.fromARGB(255, 61, 9, 97)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
