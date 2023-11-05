97119380fd754b76b5751678432fa46e6611d5bc
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2023-11-05 18:09:21 +0100

    added some variables to improve structure

--- lib/gradiant_container.dart
+++ lib/gradiant_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:dices/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 // Class for a custome widget to capsulate the color and text logic.
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -8,14 +11,14 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 27, 5, 65),
             Color.fromARGB(255, 61, 9, 97)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
